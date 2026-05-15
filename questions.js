e9b187ee2d09ce9e3030dce2d15fdd6e14bd68cf
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-05-15 10:11:40 -0400

    Modifier Q36 — Mettre en relation des faits

--- questions.js
+++ questions.js
@@ -532,6 +532,28 @@ const REGLETTES = {
       "0 point"
     ],
     niveaux: []
+  },
+  "Q36": {
+    oi: "Mettre en relation des faits",
+    colonnes: [
+      "2 points",
+      "1 point",
+      "0 point"
+    ],
+    niveaux: [
+      {
+        pts: 2,
+        desc: "L'élève met en relation tous les faits. (2 sur 2)"
+      },
+      {
+        pts: 1,
+        desc: "L'élève met en relation certains faits. (1 sur 2)"
+      },
+      {
+        pts: 0,
+        desc: "L'élève ne met pas en relation les faits. (0 sur 2)"
+      }
+    ]
   }
 }
 
