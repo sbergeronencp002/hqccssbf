5434c1f0cc002cf024f1c6b9339ecef23635b545
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-05-17 17:11:57 -0400

    Modifier Q40 — Mettre en relation des faits

--- questions.js
+++ questions.js
@@ -2973,6 +2973,7 @@ const QUESTIONS = [
     oi: "Mettre en relation des faits",
     periode: "P1 — Des origines à 1608",
     points: 2,
+    soustag: "4 éléments",
     enonce: "Les documents A à D présentent des faits sur la prise de décision chez les Iroquoiens et les Algonquiens vers 1500. Indiquez les lettres des documents qui correspondent à chacun des groupes.",
     aspects: [
       {
