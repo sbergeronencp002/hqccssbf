09048ea0a341b12b387c004fa0ebafcfde375b91
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-05-17 17:11:41 -0400

    Modifier Q37 — Mettre en relation des faits

--- questions.js
+++ questions.js
@@ -2799,6 +2799,7 @@ const QUESTIONS = [
     oi: "Mettre en relation des faits",
     periode: "P1 — Des origines à 1608",
     points: 2,
+    soustag: "2 éléments",
     enonce: "À partir des documents A et B, indiquez la lettre du document qui fait références à l'organisation territoriale des groupes autochtones.",
     aspects: [
       {
