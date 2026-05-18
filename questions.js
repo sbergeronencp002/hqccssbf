8cb1b0ed8273214b2ef42b6c7fb33b9039133d5b
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-05-18 15:09:55 -0400

    Modifier Q37 — Mettre en relation des faits

--- questions.js
+++ questions.js
@@ -3338,8 +3338,8 @@ const QUESTIONS = [
     reponse: {
       type: "mettre-en-relation",
       elements: [
-        "",
-        ""
+        "Algonquiens",
+        "Iroquoiens"
       ],
       double: true
     },
