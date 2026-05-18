8176c9b07518f05cf84e1f7498c4f7820027b0d1
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-05-18 18:16:26 -0400

    Ajouter Q78 — Situer dans l'espace (P2 — 1608 – 1760)

--- questions.js
+++ questions.js
@@ -1,5 +1,5 @@
 const REGLETTES = {
-  Q1: {
+  "Q1": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -16,7 +16,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q2: {
+  "Q2": {
     oi: "Situer dans le temps",
     colonnes: [
       "2 points",
@@ -33,7 +33,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q3: {
+  "Q3": {
     oi: "Situer dans le temps",
     colonnes: [
       "1 point",
@@ -50,7 +50,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q4: {
+  "Q4": {
     oi: "Situer dans le temps",
     colonnes: [
       "2 points",
@@ -72,7 +72,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q5: {
+  "Q5": {
     oi: "Situer dans l'espace",
     colonnes: [
       "1 point",
@@ -89,7 +89,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q6: {
+  "Q6": {
     oi: "Dégager des différences et des similitudes",
     variante: "acteur-positions",
     colonnes: [
@@ -100,7 +100,7 @@ const REGLETTES = {
     ],
     niveaux: []
   },
-  Q7: {
+  "Q7": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -122,7 +122,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q8: {
+  "Q8": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -144,7 +144,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q9: {
+  "Q9": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -166,7 +166,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q10: {
+  "Q10": {
     oi: "Déterminer des changements et des continuités",
     colonnes: [
       "2 points",
@@ -188,7 +188,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q11: {
+  "Q11": {
     oi: "Déterminer des changements et des continuités",
     colonnes: [
       "2 points",
@@ -210,7 +210,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q12: {
+  "Q12": {
     oi: "Déterminer des changements et des continuités",
     variante: "changement-continuité",
     colonnes: [
@@ -221,7 +221,7 @@ const REGLETTES = {
     ],
     niveaux: []
   },
-  Q13: {
+  "Q13": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -243,7 +243,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q14: {
+  "Q14": {
     oi: "Établir des liens de causalité",
     variante: "3 éléments — 2 liens",
     colonnes: [
@@ -254,7 +254,7 @@ const REGLETTES = {
     ],
     niveaux: []
   },
-  Q15: {
+  "Q15": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -271,7 +271,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q16: {
+  "Q16": {
     oi: "Situer dans le temps",
     colonnes: [
       "2 points",
@@ -288,7 +288,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q17: {
+  "Q17": {
     oi: "Situer dans le temps",
     colonnes: [
       "1 point",
@@ -305,7 +305,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q18: {
+  "Q18": {
     oi: "Situer dans le temps",
     colonnes: [
       "2 points",
@@ -327,7 +327,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q19: {
+  "Q19": {
     oi: "Situer dans l'espace",
     colonnes: [
       "1 point",
@@ -344,7 +344,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q20: {
+  "Q20": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -366,7 +366,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q21: {
+  "Q21": {
     oi: "Dégager des différences et des similitudes",
     variante: "acteur-positions",
     colonnes: [
@@ -377,7 +377,7 @@ const REGLETTES = {
     ],
     niveaux: []
   },
-  Q22: {
+  "Q22": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -399,7 +399,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q33: {
+  "Q33": {
     oi: "Situer dans l'espace",
     colonnes: [
       "1 point",
@@ -416,7 +416,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q37: {
+  "Q37": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -438,7 +438,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q29: {
+  "Q29": {
     oi: "Établir des liens de causalité",
     variante: "3 éléments — 2 liens",
     colonnes: [
@@ -449,7 +449,7 @@ const REGLETTES = {
     ],
     niveaux: []
   },
-  Q28: {
+  "Q28": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -471,7 +471,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q27: {
+  "Q27": {
     oi: "Déterminer des changements et des continuités",
     variante: "changement-continuité",
     colonnes: [
@@ -482,7 +482,7 @@ const REGLETTES = {
     ],
     niveaux: []
   },
-  Q23: {
+  "Q23": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -504,7 +504,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q24: {
+  "Q24": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -526,7 +526,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q25: {
+  "Q25": {
     oi: "Déterminer des changements et des continuités",
     colonnes: [
       "2 points",
@@ -548,7 +548,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q26: {
+  "Q26": {
     oi: "Déterminer des changements et des continuités",
     colonnes: [
       "2 points",
@@ -570,7 +570,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q30: {
+  "Q30": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -587,7 +587,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q31: {
+  "Q31": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -604,7 +604,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q32: {
+  "Q32": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -621,7 +621,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q34: {
+  "Q34": {
     oi: "Situer dans l'espace",
     colonnes: [
       "1 point",
@@ -638,7 +638,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q35: {
+  "Q35": {
     oi: "Situer dans l'espace",
     colonnes: [
       "1 point",
@@ -655,7 +655,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q36: {
+  "Q36": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -677,7 +677,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q38: {
+  "Q38": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -699,7 +699,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q39: {
+  "Q39": {
     oi: "Dégager des différences et des similitudes",
     variante: "acteur-positions",
     colonnes: [
@@ -710,7 +710,7 @@ const REGLETTES = {
     ],
     niveaux: []
   },
-  Q40: {
+  "Q40": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -732,7 +732,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q41: {
+  "Q41": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -749,7 +749,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q42: {
+  "Q42": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -771,7 +771,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q43: {
+  "Q43": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -793,7 +793,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q44: {
+  "Q44": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -810,7 +810,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q45: {
+  "Q45": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -827,7 +827,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q46: {
+  "Q46": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -849,7 +849,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q47: {
+  "Q47": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -866,7 +866,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q48: {
+  "Q48": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -888,7 +888,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q49: {
+  "Q49": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -905,7 +905,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q50: {
+  "Q50": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -922,7 +922,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q51: {
+  "Q51": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -939,7 +939,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q52: {
+  "Q52": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -961,7 +961,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q53: {
+  "Q53": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -983,7 +983,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q54: {
+  "Q54": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -1000,7 +1000,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q55: {
+  "Q55": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -1022,7 +1022,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q56: {
+  "Q56": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -1044,7 +1044,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q57: {
+  "Q57": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -1061,7 +1061,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q58: {
+  "Q58": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -1083,7 +1083,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q59: {
+  "Q59": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -1105,7 +1105,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q60: {
+  "Q60": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -1127,7 +1127,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q61: {
+  "Q61": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -1149,7 +1149,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q62: {
+  "Q62": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -1166,7 +1166,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q63: {
+  "Q63": {
     oi: "Établir des faits",
     colonnes: [
       "1 point",
@@ -1183,7 +1183,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q64: {
+  "Q64": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -1205,7 +1205,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q65: {
+  "Q65": {
     oi: "Dégager des différences et des similitudes",
     colonnes: [
       "2 points",
@@ -1227,7 +1227,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q66: {
+  "Q66": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -1249,7 +1249,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q67: {
+  "Q67": {
     oi: "Établir des liens de causalité",
     variante: "3 éléments — 2 liens",
     colonnes: [
@@ -1260,7 +1260,7 @@ const REGLETTES = {
     ],
     niveaux: []
   },
-  Q68: {
+  "Q68": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -1282,7 +1282,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q69: {
+  "Q69": {
     oi: "Déterminer des causes et des conséquences",
     colonnes: [
       "2 points",
@@ -1304,7 +1304,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q70: {
+  "Q70": {
     oi: "Situer dans le temps",
     colonnes: [
       "1 point",
@@ -1321,7 +1321,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q71: {
+  "Q71": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -1343,7 +1343,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q72: {
+  "Q72": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -1365,7 +1365,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q73: {
+  "Q73": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -1387,7 +1387,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q74: {
+  "Q74": {
     oi: "Mettre en relation des faits",
     colonnes: [
       "2 points",
@@ -1409,7 +1409,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q75: {
+  "Q75": {
     oi: "Situer dans le temps",
     colonnes: [
       "1 point",
@@ -1426,7 +1426,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q76: {
+  "Q76": {
     oi: "Situer dans le temps",
     colonnes: [
       "1 point",
@@ -1443,7 +1443,7 @@ const REGLETTES = {
       }
     ]
   },
-  Q77: {
+  "Q77": {
     oi: "Situer dans le temps",
     colonnes: [
       "2 points",
@@ -1459,8 +1459,25 @@ const REGLETTES = {
         desc: "L'élève ne situe pas tous les faits dans le temps."
       }
     ]
+  },
+  "Q78": {
+    oi: "Situer dans l'espace",
+    colonnes: [
+      "1 point",
+      "0 point"
+    ],
+    niveaux: [
+      {
+        pts: 1,
+        desc: "L'élève situe le fait dans l'espace."
+      },
+      {
+        pts: 0,
+        desc: "L'élève ne situe pas le fait dans l'espace."
+      }
+    ]
   }
-};
+}
 
 const IMAGE_DB = {
   "fondation_quebec_1608.png": {
@@ -1586,7 +1603,7 @@ const IMAGE_DB = {
   "ligne_1475-1575.png": {
     src: "images/ligne_1475-1575.png"
   }
-};
+}
 
 const QUESTIONS = [
   {
@@ -4856,5 +4873,35 @@ const QUESTIONS = [
       ref: "ligne_acBd.png"
     },
     guide: "C - A - B - D"
+  },
+  {
+    id: "Q78",
+    niveau: 3,
+    oi: "Situer dans l'espace",
+    periode: "P2 — 1608 – 1760",
+    points: 1,
+    soustag: "Situer 1 élément",
+    enonce: "À partir du document A, indiquez la lettre qui correspond au territoire sur lequel les seigneuries ont été concédées avant 1663.",
+    aspects: [
+      {
+        aspect: "Régime seigneurial"
+      }
+    ],
+    documents: [
+      {
+        type: "textes",
+        cols: [
+          {
+            titre: "Document A",
+            ref: "carte_seigneurie_n-f.png",
+            source: "MEES."
+          }
+        ]
+      }
+    ],
+    reponse: {
+      type: "tableau_2col"
+    },
+    guide: "B."
   }
-];
+]
