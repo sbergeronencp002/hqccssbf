c1ce6d31da6f488c67fe3ab615296bccab2a7f37
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-05-19 07:14:19 -0400

    Modifier Q58 — Déterminer des causes et des conséquences

--- questions.js
+++ questions.js
@@ -4120,7 +4120,7 @@ const QUESTIONS = [
     periode: "P3 — 1760 – 1791",
     points: 2,
     soustag: "Cause",
-    enonce: "À partir du document A, indiquez une cause du rappel de James Murray en Angleterre.",
+    enonce: "indiquez une cause du rappel de James Murray en Angleterre au début du Régime britannique.",
     aspects: [
       {
         aspect: "Mouvements de revendication"
