42f61593e5639d7e5db5c59e836910456d0d0a67
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-05-21 11:20:58 -0400

    Modifier Q102 — Mettre en relation des faits

--- questions.js
+++ questions.js
@@ -6733,6 +6733,7 @@ const QUESTIONS = [
     oi: "Mettre en relation des faits",
     periode: "P4 — 1791 – 1840",
     points: 2,
+    soustag: "3 éléments",
     enonce: "Les documents A à C présentent les réactions de différents groupes à la suite du Rapport Durham. Associez chaque position au groupe correspondant.",
     aspects: [
       {
