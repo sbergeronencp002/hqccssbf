54f8c335def65b340b67c328b2f658f299099a86
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-06-11 20:20:45 -0400

    Modifier Q367 — Déterminer des causes et des conséquences

--- questions.js
+++ questions.js
@@ -24016,6 +24016,7 @@ const QUESTIONS = [
           {
             titre: "Document B",
             ref: "population_bas-canada_1800.png",
+            soustitre: "Répartition de la population du Bas-Canada vers 1800",
             source: "Adapté de Fernand Ouellet, Le Bas-Canada 1791-1840 : changements structuraux et crise, Ottawa, Éditions de l'Université d'Ottawa, 1976."
           }
         ]
@@ -24037,7 +24038,7 @@ const QUESTIONS = [
         ]
       ]
     },
-    updatedAt: "2026-06-11T12:27:10.402Z"
+    updatedAt: "2026-06-12T00:20:41.050Z"
   },
   {
     id: "Q368",
