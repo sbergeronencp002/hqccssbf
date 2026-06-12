238c4912d7f147b7cc27f1e76826dba55261f135
sbergeronencp002 <sbergeron.ens@gmail.com>
2026-06-12 07:48:50 -0400

    Modifier Q329 — Déterminer des changements et des continuités

--- questions.js
+++ questions.js
@@ -22641,7 +22641,7 @@ const QUESTIONS = [
           {
             titre: "Document A",
             ref: "org_pol_1774_acte_quebec.png",
-            soustitre: "1774",
+            soustitre: "Organisation politique de la colonie entre 1774 et 1791",
             source: "Reconstitution historique à des fins pédagogiques réalisée à l’aide de l’intelligence artificielle (ChatGPT/OpenAI), 2026."
           }
         ]
@@ -22652,6 +22652,7 @@ const QUESTIONS = [
           {
             titre: "Document B",
             ref: "org_pol_1791_acte_const.png",
+            soustitre: "Organisation politique de la colonie à partir de 1791",
             source: "Reconstitution historique à des fins pédagogiques réalisée à l’aide de l’intelligence artificielle (ChatGPT/OpenAI), 2026."
           }
         ]
@@ -22662,7 +22663,7 @@ const QUESTIONS = [
       nombre: 3
     },
     guide: "Changement\nIl y a changement, car en 1791, l’Acte constitutionnel crée une Chambre d’assemblée élue par la population et divise la colonie en Haut-Canada et Bas-Canada, alors qu’en 1774, la Province de Québec ne possède pas de Chambre d’assemblée et forme une seule colonie.\n\nou \n\nIl y a changement, car en 1791, chaque colonie possède une Chambre d’assemblée élue, tandis qu’en 1774, le gouverneur dirige avec le Conseil exécutif et le Conseil législatif sans assemblée élue.\n\nContinuité\nIl y a continuité, car en 1774 comme en 1791, le gouverneur demeure nommé par les autorités britanniques et conserve un rôle important dans l'administration de la colonie.",
-    updatedAt: "2026-06-09T16:23:27.662Z"
+    updatedAt: "2026-06-12T11:48:46.516Z"
   },
   {
     id: "Q330",
